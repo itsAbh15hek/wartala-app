2d7d6e889a0a0c36ba1dfafd2947d43d5ab5c81e
Abh15hek <itsabh15hek@outlook.com>
2021-09-08 20:57:44 +0530

    added firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app'
+
+const config:{
+  apiKey: "AIzaSyB10HpW5DDzzm9k9Btjm1oB7ufGwbAJ1bs",
+  authDomain: "wartala-app.firebaseapp.com",
+  projectId: "wartala-app",
+  storageBucket: "wartala-app.appspot.com",
+  messagingSenderId: "4598573577",
+  appId: "1:4598573577:web:3fa41830d490357a6526d8"
+};
