e9a7b2adbbdb76eb56b0bf2f2eee28c36734f225
Abh15hek <itsabh15hek@outlook.com>
2021-09-10 18:55:12 +0530

    created private and public routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
+import SignIn from './pages/SignIn';
 import './styles/main.scss';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>d</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,10 +1,18 @@
-import firebase from 'firebase/app'
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const config:{
-  apiKey: "AIzaSyB10HpW5DDzzm9k9Btjm1oB7ufGwbAJ1bs",
-  authDomain: "wartala-app.firebaseapp.com",
-  projectId: "wartala-app",
-  storageBucket: "wartala-app.appspot.com",
-  messagingSenderId: "4598573577",
-  appId: "1:4598573577:web:3fa41830d490357a6526d8"
+// Your web app's Firebase configuration
+const config = {
+  apiKey: 'AIzaSyB10HpW5DDzzm9k9Btjm1oB7ufGwbAJ1bs',
+  authDomain: 'wartala-app.firebaseapp.com',
+  projectId: 'wartala-app',
+  storageBucket: 'wartala-app.appspot.com',
+  messagingSenderId: '4598573577',
+  appId: '1:4598573577:web:3fa41830d490357a6526d8',
 };
+
+// Initialize Firebase
+const app = firebase.initializeApp(config);
